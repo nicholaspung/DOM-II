a038e9fd27f4a90eaae04643ea7b35b530f3c1b1
Nicholas Pung <nicholaspung>
2019-05-14 13:25:52 -0700

    added keystrokes to site

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 // Your code goes here
 
+// courtesy of Samantha for showcasing this
+// changes logo background from green to white
 const logo = document.querySelector(".logo-heading");
 
-// courtesy of Samantha for showcasing this
 logo.addEventListener("mouseover", event => {
     let backgroundProp = event.target.style.background;
     event.target.style.background = backgroundProp == "green" ? "white" : "green";
 });
 
+// adds keystrokes to bottom of header paragraph
+const p = document.createElement("p");
+
+const funBusHeader = document.querySelector(".intro");
+funBusHeader.appendChild(p);
+
+document.addEventListener("keydown", event => {
+    p.textContent += `${event.code} `;
+});
