dd5a5d6ab9788b07f9d7b5cadeac5d2d1bbf46f9
Nicholas Pung <nicholaspung>
2019-05-14 12:42:55 -0700

    added 1 event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+const logo = document.querySelector(".logo-heading");
+
+// courtesy of Samantha for showcasing this
+logo.addEventListener("mouseover", event => {
+    let backgroundProp = event.target.style.background;
+    event.target.style.background = backgroundProp == "green" ? "white" : "green";
+});
+
